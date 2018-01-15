ac511d84486f611014a3aca33037a109dcd6bc8d
Manuel Antonio Gomez Angulo <manuel.pc23@gmail.com>
2018-01-15 13:46:37 +0100

    FIxed(MisMatematicas) Import de logback.classic

--- src/main/java/es/us/egc/MisMatematicas.java
+++ src/main/java/es/us/egc/MisMatematicas.java
@@ -1,7 +1,8 @@
 package es.us.egc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
 /**
  * DIVISION
  *
